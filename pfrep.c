be8148d98dd2e00390e76b052b3ab47d28f25145
EstradaCode <franciscoestrada565@gmail.com>
2026-04-04 18:30:18 -0300

    pfrep.c added toq15 && to1615

--- pfrep.c
+++ pfrep.c
@@ -45,6 +45,7 @@ int16_t str_to_q78(const char* s) {
     return (signo == -1)? -resultado : resultado;
 
 }
+
 void q78_to_hex(int16_t val, char* out) {
     const char hex_chars[] = "0123456789ABCDEF";
     uint16_t uval = (uint16_t)val; //
@@ -56,4 +57,74 @@ void q78_to_hex(int16_t val, char* out) {
     out[4] = hex_chars[(uval >> 4) & 0xF];  //
     out[5] = hex_chars[uval & 0xF];         // (LSB)
     out[6] = '\0';
+}
+
+int16_t str_to_q15(const char* s) {
+    int32_t parte_entera = 0;
+    uint32_t parte_fraccionaria = 0;
+    uint32_t divisor_escalonado = 1;
+    int signo = 1;
+    int i = 0;
+
+    if (s[i] == '-') { signo = -1; i++; }
+    else if (s[i] == '+') { i++; }
+
+    while (s[i] != '.' && s[i] != '\0') {
+        parte_entera = parte_entera * 10 + (s[i++] - '0'); 
+    }
+
+    if (parte_entera > 1) {
+        fprintf(stderr, "Error: m fuera de rango para Q0.15\n");
+        exit(1);
+    }
+
+    if (s[i] == '.') {
+        i++;
+        while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9') {
+            parte_fraccionaria = parte_fraccionaria * 10 + (s[i++] - '0');
+            divisor_escalonado *= 10;
+        }
+        parte_fraccionaria = (parte_fraccionaria << 15) / divisor_escalonado;
+    }
+
+    uint16_t res_abs = (uint16_t)((parte_entera << 15) | (parte_fraccionaria & 0x7FFF));
+    
+    int16_t resultado = (int16_t)res_abs;
+    return (signo == -1) ? -resultado : resultado;
+}
+int32_t str_to_q1615(const char* s) {
+    int32_t parte_entera = 0;
+    uint64_t parte_fraccionaria = 0; 
+    uint32_t divisor_escalonado = 1;
+    int signo = 1;
+    int i = 0;
+
+    if (s[i] == '-') { signo = -1; i++; }
+    else if (s[i] == '+') { i++; }
+
+    
+    while (s[i] != '.' && s[i] != '\0') {
+        if (s[i] >= '0' && s[i] <= '9') {
+            parte_entera = parte_entera * 10 + (s[i++] - '0');
+        } else break;
+    }
+
+    if (parte_entera > 32767) {
+        fprintf(stderr, "Error: x fuera de rango para Q16.15\n");
+        exit(1);
+    }
+
+    if (s[i] == '.') {
+        i++;
+        while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9') {
+            parte_fraccionaria = parte_fraccionaria * 10 + (s[i++] - '0');
+            divisor_escalonado *= 10;
+        }
+        parte_fraccionaria = (parte_fraccionaria << 15) / divisor_escalonado;
+    }
+
+    uint32_t res_abs = (uint32_t)((parte_entera << 15) | (parte_fraccionaria & 0x7FFF));
+    
+    int32_t resultado = (int32_t)res_abs;
+    return (signo == -1) ? -resultado : resultado;
 }
